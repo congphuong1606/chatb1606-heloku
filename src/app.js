93d5944dfb11656576deb726b2a29138e484f133
congphuong1606 <cphuong.coldboy@gmail.com>
2021-01-01 01:01:50 +0700

    ffff

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const token = '1423009565:AAGfe6iyl-8HbjIACMZSa_DMfFH051TWt5A'; //chinh
-//const token = '1423009565:AAGfe6iyl-8HbjIACMZSa_DMfFH051TWt5A'; //phu
+//const token = '1494741747:AAHyYTkONjsTbgaHzdfXSv17mKIqFqmg42I'; //phu
 var change_alias = require("./char.js");
 const bot = new TelegramBot(token, {
     polling: true
@@ -95,9 +95,160 @@ bot.on('callback_query', function onCallbackQuery(callbackQuery) {
         xoadonhang(msg);
         // text = 'Hãy nhập theo cú pháp: B_SốLượng_TổngTiền_Coin_KháchHàng  (vd: B 106500 24800000 USDT Hoang)';
     }
+    if (action === 'edit_order') {
+        editdonhang(msg);
+        // text = 'Hãy nhập theo cú pháp: B_SốLượng_TổngTiền_Coin_KháchHàng  (vd: B 106500 24800000 USDT Hoang)';
+    }
 
 
 });
+
+function editdonhang(msg) {
+    const fromeuser = msg.chat.username;
+    const fromIdChat = msg.chat.id;
+    if (arrayOrder != []) {
+        arrayOrder.forEach(function(item, index, object) {
+            if (item.username === fromeuser) {
+                object.splice(index, 1);
+            }
+        });
+    }
+    arrayOrder.push({
+        code: '',
+        username: fromeuser,
+        rate: '',
+        soluong: '',
+        khachhang: '',
+    })
+
+    console.log(arrayOrder);
+    bot.sendMessage(fromIdChat, 'Nhập mã đơn hàng cần chỉnh sửa', {
+        reply_markup: {
+            force_reply: true
+        }
+    }).then(payload => {
+        const replyListenerId = bot.onReplyToMessage(payload.chat.id, payload.message_id, msg => {
+            bot.removeReplyListener(replyListenerId)
+                ////////
+
+
+
+
+            if (arrayOrder != []) {
+                arrayOrder.forEach(function(item) {
+                    if (item.username === msg.from.username) {
+                        item.code = msg.text;
+                        console.log(arrayOrder);
+
+
+
+
+
+                        //nhập tổng tiền  --------------------------
+                        bot.sendMessage(fromIdChat, 'Nhập số lượng mới?', {
+                                reply_markup: {
+                                    force_reply: true
+                                }
+                            }).then(payload => {
+                                const replyListenerId = bot.onReplyToMessage(payload.chat.id, payload.message_id, msg => {
+                                    bot.removeReplyListener(replyListenerId)
+                                        // console.log(msg) // here's the reply which is I think what you want
+                                    if (arrayOrder != []) {
+                                        arrayOrder.forEach(function(item) {
+                                            if (item.username === msg.from.username) {
+                                                item.soluong = msg.text;
+                                                console.log(arrayOrder);
+
+
+
+
+
+                                                //nhập coin  --------------------------
+                                                bot.sendMessage(fromIdChat, 'Nhập rate mới', {
+                                                        reply_markup: {
+                                                            force_reply: true
+                                                        }
+                                                    }).then(payload => {
+                                                        const replyListenerId = bot.onReplyToMessage(payload.chat.id, payload.message_id, msg => {
+                                                            bot.removeReplyListener(replyListenerId)
+                                                                // console.log(msg) // here's the reply which is I think what you want
+                                                            if (arrayOrder != []) {
+                                                                arrayOrder.forEach(function(item) {
+                                                                    if (item.username === msg.from.username) {
+                                                                        item.rate = msg.text;
+                                                                        console.log(arrayOrder);
+                                                                        const urlcall = urlnhapdon + 'editdonhang' + '&user=@' + item.username + '&soluong=' + item.soluong + '&rate=' + item.rate + '&code=' + item.code;
+                                                                        console.log(urlcall);
+                                                                        const axios = require('axios');
+                                                                        axios.get(urlcall)
+                                                                            .then(response => {
+                                                                                if (response.data.result != 'Mã đơn hàng không tồn tại!') {
+                                                                                    bot.sendMessage(
+                                                                                        payload.chat.id,
+                                                                                        `Cập nhật thành công!\nMã đơn:` + item.code + `\nSố lượng:` + item.soluong +
+                                                                                        `\nRate:` + item.rate + `\nKhách hàng: ` + response.data.result + `\nCám ơn quý khách!`, {
+                                                                                            parse_mode: 'HTML',
+                                                                                        }
+                                                                                    );
+                                                                                } else {
+                                                                                    bot.sendMessage(
+                                                                                        payload.chat.id, response.data.result, {
+                                                                                            parse_mode: 'HTML',
+                                                                                        }
+                                                                                    );
+                                                                                }
+                                                                                console.log(response.data.result);
+                                                                            })
+                                                                            .catch(error => {
+                                                                                console.log(error);
+                                                                            });
+                                                                    } else {
+                                                                        console.log('có vẫn đề ở nhập coin');
+                                                                    }
+                                                                });
+                                                            } else {
+                                                                console.log('có vẫn đề ở if (arrayOrder != []) trong phần nhập coin');
+                                                            }
+                                                        })
+                                                    })
+                                                    //kết thúc nhập coin -----------------------
+
+                                            } else {
+                                                console.log('có vẫn đề ở nhập tổng tiền');
+                                            }
+                                        });
+                                    } else {
+                                        console.log('có vẫn đề ở if (arrayOrder != []) trong phần nhập tổng tiền');
+                                    }
+                                })
+                            })
+                            //kết thúc nhập tổng tiền -----------------------
+
+
+
+
+
+                    } else {
+                        console.log('có vẫn đề ở nhập số lượng');
+                    }
+                });
+            } else {
+                console.log('có vẫn đề ở if (arrayOrder != [])');
+            }
+
+
+
+
+
+
+
+            //////////
+
+
+
+        })
+    })
+}
 
 function xoadonhang(msg) {
     const fromeuser = msg.chat.username;
@@ -270,7 +421,8 @@ bot.onText(/\/start/, (msg) => {
             inline_keyboard: [
                 [{ text: 'ĐƠN MUA HÀNG', callback_data: 'buy_order' }],
                 [{ text: 'ĐƠN BÁN HÀNG', callback_data: 'sell_order' }],
-                [{ text: 'XÓA ĐƠN HÀNG', callback_data: 'delete_order' }]
+                [{ text: 'XÓA ĐƠN HÀNG', callback_data: 'delete_order' }],
+                [{ text: 'SỬA ĐƠN HÀNG', callback_data: 'edit_order' }]
             ],
             resize_keyboard: true,
             one_time_keyboard: true,
